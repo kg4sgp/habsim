550f930e2573652c8edabea4d10c27bfb29e576f
Jimmy Carter <kg4sgp@gmail.com>
2017-03-27 00:41:29 -0500

    Adding references and more to the readme

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # habsim
 High Altitude Balloon Simulator
+
+Currently this is the start of a high altitude balloon simulator.
+
+Currently:
+* Doesn't calculate changing pressure vs altitude (pretty obviously incredibly wrong)
+* Assumes constant balloon envelope
+* Several other silly things that would happen when you are just starting in on a project.
+
+To do:
+
+* Calculate changing pressure of atmosphere with altitude
+* Calculate changing drag due to changing balloon envelope
+* Calculate burst altitude from balloon envelope
+
--- habsim.c
+++ habsim.c
@@ -13,7 +13,7 @@ int main(void)
   double b_mass = 1.0;
   double b_alti = 0.0;
   double b_cd = 0.47;
-  double f_lift_kg = 1.5;
+  double f_lift_kg = 3.0;
   double b_diam = 2.0;
   
   /* calculated state variables for the balloon */
@@ -26,7 +26,7 @@ int main(void)
   double f_drag  = 0.0;
   
   /* time increment in seconds*/
-  double t_inc = 0.001;
+  double t_inc = 0.1;
   double t = 0;
   
   /* force of gravity */
@@ -46,22 +46,25 @@ int main(void)
     /* use altitude to calculate cross sectional area of balloon */
      
   
-    /* cross sectional area of sphere */
+    /* cross sectional area of sphere: A = pi*r^2 */
     b_cross_area = M_PI * pow((b_diam/2), 2);
-    /* volume of sphere */ 
+    /* volume of sphere V = 4/3 * pi *r^3 */ 
     b_volu = (4.0/3.0) * M_PI * pow((b_diam/2), 3);
     
     /* drag force */
+    /* https://en.wikipedia.org/wiki/Drag_equation */
     f_drag = (0.5)*air_den*pow(b_velo,2)*b_cd*b_cross_area;
     
     /* net forces */
     f_net = f_lift - (f_g + f_drag);
     
+    /* Kenimatic Equations */
+    /* https://en.wikipedia.org/wiki/Equations_of_motion */
     b_acel = f_net/b_mass;
     b_velo = b_velo + b_acel*t_inc;
     b_alti = b_alti + b_velo*t_inc + (0.5 * b_acel * pow(t_inc,2));
     
-    //printf("%f fnet: %f, alt: %f, vel: %f, acel: %f\n", t, f_net, b_alti, b_velo, b_acel);
+    printf("%f fnet: %f, alt: %f, vel: %f, acel: %f\n", t , f_net, b_alti, b_velo, b_acel);
 
     t += t_inc;    
    }    
