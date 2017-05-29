e5efd385c0770015f45b95dfc2b182de2f47d0de
Ricky Elrod <ricky@elrod.me>
2017-05-29 00:47:09 -0400

    Error more quickly on CSV parse fail in demo program
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

--- haskell/src/exe/HABSim.hs
+++ haskell/src/exe/HABSim.hs
@@ -21,7 +21,7 @@ main = do
       pv = PosVel 41.1063 (-80.6477) (Altitude 300) 0.0 0.0 3.0
       bv = Bvars 2.0 0.47 1.0 0.5 0.0 540.0 (Liter 5.0) 120000.0
       w = Wind 4.0 4.0
-      Right gribLines = decodeGrib csv
+      gribLines = either error id (decodeGrib csv)
       pressures = fmap (pressure . gribLineToRaw) gribLines
       (lastAscent@(Breturn sv' pv' bv' w'), accAscent) =
         runWriter $ sim Ascent sv pv bv w pressures gribLines
