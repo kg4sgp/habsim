0ac0ea796c16a82599fc33b2f85e656134cc7f2f
Ricky Elrod <ricky@elrod.me>
2017-05-29 00:29:22 -0400

    Update script to call demo with csv path
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

--- haskell/html/generate.sh
+++ haskell/html/generate.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 set -e
+
+if [[ "$1" == "" ]] || [[ ! -f "$1" ]]; then
+  echo "Argument to script must be a Grib2 CSV file."
+  exit 1
+fi
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 pushd $DIR/..
   cabal build
-  dist/build/habsim/habsim > $DIR/flight_path.js
+  dist/build/habsim/habsim "$1" > $DIR/flight_path.js
   echo "Open $DIR/index.html in your browser."
 popd
