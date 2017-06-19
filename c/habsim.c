d31499d933a111800e1638b4dcc9508825aed42c
Ricky Elrod <ricky@elrod.me>
2017-06-19 16:21:39 -0400

    nuke trailing whitespace
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

--- c/habsim.c
+++ c/habsim.c
@@ -16,13 +16,13 @@ prdn calc_pressure(double alt)
   double M = 0.0289644;
   double R = 8.3144598;
   double g = 9.80665;
-  
+
   double hb = 0.0;
   double tb = 0.0;
   double lb = 0.0;
   double pb = 0.0;
   double rho = 0.0;
-  
+
   /* set values for different altitude regions */
   if (alt < 11000.0) {
     hb = 0.0;
@@ -67,21 +67,21 @@ prdn calc_pressure(double alt)
     pb = 3.96;
     rho = 0.000064;
   }
-  
+
   /* if temp lapse rate is 0.0 use the first formula, otherwise second */
   if (lb == 0.0) {
     prdn2.pr = pb * exp( (-g * M * (alt-hb))/(R*tb));
   } else {
     prdn2.pr = pb * pow( ( tb / (tb + lb * (alt- hb) ) ), ((g*M)/(R*lb)));
   }
-  
+
   if (lb == 0.0) {
     prdn2.dn = rho * exp( (-g * M * (alt-hb))/(R*tb));
   } else {
     prdn2.dn = rho * pow( ( tb / (tb + lb * (alt- hb) ) ), (1.0+((g*M)/(R*lb))));
   }
-  
-  
+
+
   return prdn2;
 }
 
@@ -117,8 +117,8 @@ double calc_temp(double alt)
 double gas_dens(double molmass, double pressure, double temperature)
 {
   /* universal gas constant */
-  double r = 8.3144598; 
-  
+  double r = 8.3144598;
+
   double gd = ((molmass*pressure)/(r*temperature));
   return gd;
 }
@@ -152,7 +152,7 @@ int main(void)
   double land_ele = 300.0;
   double f_boyant = 0.0;
   double f_boyant_a = 0.0;
-  
+
   /* calculated state variables for the balloon */
   double b_velo = 0.0;
   double b_acel = 0.0;
@@ -166,7 +166,7 @@ int main(void)
   double f_drag  = 0.0;
   double a_temp = 0.0;
   double a_temp_n = 0.0;
-  prdn prdn1;  
+  prdn prdn1;
   /* time increment in seconds*/
   double t_inc = 0.01;
   double t = 0.0;
@@ -187,15 +187,15 @@ int main(void)
   double f_drag_y = 0.0;
   double f_net_x = 0.0;
   double f_net_y = 0.0;
-  
+
   /* Simulation results */
   double max_alti = 0.0;
-  
+
   /* calculate some inital things*/
   b_alti = land_ele;
   double f_g = b_mass * g;
   f_lift = f_lift_kg * g;
-  /* volume of sphere V = 4/3 * pi *r^3 */ 
+  /* volume of sphere V = 4/3 * pi *r^3 */
   b_volu = (4.0/3.0) * M_PI * pow((b_diam/2), 3);
   b_volu_n = b_volu;
   prdn1 = calc_pressure(b_alti);
@@ -203,70 +203,70 @@ int main(void)
   a_temp = calc_temp(b_alti);
   double gamma = 7.0/5.0;
   adabatic_const = pow(b_pres, (1-gamma)) * pow(a_temp, gamma);
-  
-  
+
+
   /* print flight parameters */
   printf("\n");
   printf("Total Mass: %.3fkg\n", b_mass);
   printf("Total Lift: %.3f N (%.3f kg)\n", f_lift, f_lift_kg);
   printf("Diameter: %.3f m\n", b_diam);
   printf("\n");
-  
+
   // printf("seconds, fnet, fdrag, altitude, velocity, acceleration, pressure, radius, volume\n");
   while(b_radius < b_burst_dia) {
-  
+
     /* calculate pressure at altitude */
     prdn prdn_new = calc_pressure(b_alti);
-  
+
     a_temp_n = calc_temp(b_alti);
-  
+
     /* calculate new voume of balloon */
     b_volu_n = (b_pres*b_volu_n)/prdn_new.pr;
     b_volu = (b_pres*b_volu*a_temp_n)/(a_temp*prdn_new.pr);
     b_pres = prdn_new.pr;
-    
+
     /* calculate radius from volume */
     b_radius = cbrt((3.0*b_volu)/(4.0*M_PI));
-  
+
     /* cross sectional area of sphere: A = pi*r^2 */
     b_cross_area = M_PI * pow(b_radius, 2);
-    
+
     a_temp = calc_temp(b_alti);
     adabatic_temp = pow( (adabatic_const / pow(b_pres, (1-gamma)) ), (1/gamma));
-    
+
     /* calculate h2 density */
     h2_den = gas_dens(mm_h2, b_pres, a_temp);
-    
+
     /* calculate boyant force */
     f_boyant = boyancy(prdn_new.dn, h2_den, g, b_volu);
     f_boyant_a = boyancy(prdn_new.dn, gas_dens(mm_h2, b_pres, a_temp-10.0), g, b_volu);
-    
+
     /* drag force - ascending balloon*/
     /* https://en.wikipedia.org/wiki/Drag_equation */
     f_drag = (0.5)*prdn_new.dn*pow(b_velo,2)*b_cd*b_cross_area;
     f_drag_x = (0.5)*prdn_new.dn*pow((w_velo_x-b_velo_x),2)*lat_cd*b_cross_area;
     f_drag_y = (0.5)*prdn_new.dn*pow((w_velo_y-b_velo_y),2)*lat_cd*b_cross_area;
-    
+
     /* net forces */
     f_net = f_boyant_a - (f_g + f_drag);
     f_net_x = f_drag_x;
     f_net_y = f_drag_y;
 
-    
+
     /* Kenimatic Equations */
     /* https://en.wikipedia.org/wiki/Equations_of_motion */
     b_acel = f_net/b_mass;
     b_velo = b_velo + b_acel*t_inc;
     b_alti = b_alti + b_velo*t_inc + (0.5 * b_acel * pow(t_inc,2));
-    
+
     b_acel_x = f_net_x/b_mass;
     b_velo_x = b_velo_x + b_acel_x*t_inc;
     b_disp_x = b_disp_x + b_velo_x*t_inc + (0.5 * b_acel_x * pow(t_inc,2));
-    
+
     b_acel_y = f_net_y/b_mass;
     b_velo_y = b_velo_y + b_acel_y*t_inc;
     b_disp_y = b_disp_y + b_velo_y*t_inc + (0.5 * b_acel_y * pow(t_inc,2));
-    
+
     print_count++;
     if (print_count == print_deci){
       //printf("%.1f, %f, %f, %f, %f, %f, %f\n", t, b_alti, b_pres, a_temp, h2_den, prdn_new.dn, f_boyant);
@@ -275,64 +275,64 @@ int main(void)
       printf("%.1f, %.2f %.3f, %.3f, %.3f, %.3f\n", t, b_alti, b_velo, b_volu, b_volu_n, b_volu - b_volu_n);
       print_count = 0;
     }
-    
+
     if (b_alti > max_alti) max_alti = b_alti;
 
-    t += t_inc;    
-   }   
-  
+    t += t_inc;
+   }
+
   double t_ascent = t;
   double ahours = t_ascent/3600.0;
   double amin = (ahours - floor(ahours))*60;
   double asec = (amin - floor(amin))*60;
-  
+
   /* calculate decent phase of flight */
   print_count = 0;
-  
+
   while(b_alti > land_ele) {
     /* calculate pressure at altitude */
     prdn prdn_new = calc_pressure(b_alti);
-    
+
     /* drag force - parachute*/
     /* https://en.wikipedia.org/wiki/Drag_equation */
     f_drag = (0.5)*prdn_new.dn*pow(b_velo,2)*p_cd*p_cross_area;
-    
+
     /* net forces */
     f_net = f_drag - f_g;
-    
+
     a_temp = calc_temp(b_alti);
-    
+
     /* Kenimatic Equations */
     /* https://en.wikipedia.org/wiki/Equations_of_motion */
     b_acel = f_net/b_mass;
     b_velo = b_velo + b_acel*t_inc;
     b_alti = b_alti + b_velo*t_inc + (0.5 * b_acel * pow(t_inc,2));
-    
+
     print_count++;
     if (print_count == print_deci){
       //printf("%.1f, %f, %f, %f, %f, %f, %f, %f\n", t , f_net, f_drag, b_alti, b_velo, b_acel, b_pres, a_temp);
       print_count = 0;
     }
-    
-    t += t_inc;   
-    
+
+    t += t_inc;
+
   }
-  
+
   double t_decent = t - t_ascent;
   double dhours = t_decent/3600.0;
   double dmin = (dhours - floor(dhours))*60;
   double dsec = (dmin - floor(dmin))*60;
-  
+
   double t_total = t;
   double hours = t_total/3600.0;
   double min = (hours - floor(hours))*60;
   double sec = (min - floor(min))*60;
-  
+
   printf("Maximum Altitude: %.1f meters.\n", max_alti);
   printf("Ascent duration %.0f hours, %.0f min, %.2f sec.\n", floor(ahours), floor(amin), asec);
   printf("Decent duration %.0f hours, %.0f min, %.2f sec.\n", floor(dhours), floor(dmin), dsec);
   printf("Flight duration %.0f hours, %.0f min, %.2f sec.\n", floor(hours), floor(min), sec);
-    
+
   printf("\n");
   return 0;
 }
